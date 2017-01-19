ad25b83f9a9c4636f05c62b200e6b14a8e9eaccc
novemberizing <novemberizing@gmail.com>
2017-01-19 20:50:26 +0900

    remove cyclable in executor

--- src/main/java/novemberizing/rx/Req.java
+++ src/main/java/novemberizing/rx/Req.java
@@ -12,49 +12,49 @@ import novemberizing.util.Log;
  * @author novemberizing, me@novemberizing.net
  * @since 2017. 1. 17.
  */
-public class Req<T, Z> {
+public class Req<T> {
     private static final String Tag = "Req";
-    private Task<T, Z> __task;
+    private Task<Req<T>, T> __task;
 
     boolean executed(){ return __task!=null && __task.executed(); }
 
     boolean completed(){return __task!=null && __task.completed(); }
 
-    public Observable<Z> subscribe(Observer<Z> observer){
+    public Observable<T> subscribe(Observer<T> observer){
         Log.e(Tag, "not requested");
         return __task!=null ? __task.subscribe(observer) : null;
     }
 
 
-    public Observable<Z> unsubscribe(Observer<Z> observer){
+    public Observable<T> unsubscribe(Observer<T> observer){
 
         return __task!=null ? __task.unsubscribe(observer) : null;
     }
-    public Observable<Z> unsubscribe(){
+    public Observable<T> unsubscribe(){
         return __task!=null ? __task.unsubscribe() : null;
     }
 
 
-    public <U> Sync<Z, U> sync(Single<Z, U> f){
+    public <U> Sync<T, U> sync(Single<T, U> f){
         return __task!=null ? __task.sync(f) : null;
     }
-    public <U> Sync<Z, U> sync(novemberizing.ds.on.Pair<Operator.Task<Z, U>, Z> f){
+    public <U> Sync<T, U> sync(novemberizing.ds.on.Pair<Operator.Task<T, U>, T> f){
         return __task!=null ? __task.sync(f) : null;
     }
 
-    public <U, V> Composer<Z, U, V> compose(Observable<U> secondary, novemberizing.ds.func.Pair<Z, U, V> f){
+    public <U, V> Composer<T, U, V> compose(Observable<U> secondary, novemberizing.ds.func.Pair<T, U, V> f){
         return __task!=null ? __task.compose(secondary, f) : null;
     }
 
-    public <U, V> Condition<Z, U, V> condition(Observable<U> observable, novemberizing.ds.func.Pair<Z, U, Boolean> condition , novemberizing.ds.func.Pair<Z, U, V> f){
+    public <U, V> Condition<T, U, V> condition(Observable<U> observable, novemberizing.ds.func.Pair<T, U, Boolean> condition , novemberizing.ds.func.Pair<T, U, V> f){
         return __task!=null ? __task.condition(observable, condition, f) : null;
     }
 
-    public <U> Operator<Z, U> condition(Single<Z, Boolean> condition, Single<Z, U> f){
+    public <U> Operator<T, U> condition(Single<T, Boolean> condition, Single<T, U> f){
         return __task!=null ? __task.condition(condition, f) : null;
     }
 
-    public Observable<Z> replay(int limit){
+    public Observable<T> replay(int limit){
         return __task!=null ? __task.replay(limit) : null;
     }
 }
