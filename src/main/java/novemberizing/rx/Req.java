27209c69eea02a57344670a88b9c35ce1561f884
novemberizing <novemberizing@gmail.com>
2017-01-20 12:12:44 +0900

    Implement Req

--- src/main/java/novemberizing/rx/Req.java
+++ src/main/java/novemberizing/rx/Req.java
@@ -9,10 +9,10 @@ import novemberizing.ds.Tuple;
  * @author novemberizing, me@novemberizing.net
  * @since 2017. 1. 20.
  */
-public class Req<T extends Tuple, Z> {
-    private Observable<Z> __observable;
-    private novemberizing.ds.Exec __req;
-    private novemberizing.ds.on.Single<Z> __ret = new novemberizing.ds.on.Single<Z>(){
+public class Req<Z> {
+    private final Observable<Z> __observable;
+    private final novemberizing.ds.Exec __req;
+    private final novemberizing.ds.on.Single<Z> __ret = new novemberizing.ds.on.Single<Z>(){
         @Override
         public void on(Z o) {
             try {
@@ -23,40 +23,47 @@ public class Req<T extends Tuple, Z> {
         }
     };
 
-    public Req(novemberizing.ds.on.Single<novemberizing.ds.on.Single<Z>> on){
+    public Req(Observable<Z> observable, novemberizing.ds.on.Single<novemberizing.ds.on.Single<Z>> on){
+        __observable = observable;
         __req = new On.Exec.Empty<>(on, __ret);
     }
 
-    public <A> Req(A first, novemberizing.ds.on.Pair<A, novemberizing.ds.on.Single<Z>> on){
+    public <A> Req(Observable<Z> observable, A first, novemberizing.ds.on.Pair<A, novemberizing.ds.on.Single<Z>> on){
+        __observable = observable;
         __req = new On.Exec.Single<>(Tuple.New(first), on, __ret);
     }
 
-    public <A, B> Req(A first, B second, novemberizing.ds.on.Triple<A, B, novemberizing.ds.on.Single<Z>> on){
+    public <A, B> Req(Observable<Z> observable, A first, B second, novemberizing.ds.on.Triple<A, B, novemberizing.ds.on.Single<Z>> on){
+        __observable = observable;
         __req = new On.Exec.Pair<>(Tuple.New(first, second), on, __ret);
     }
 
-    public <A, B, C> Req(A first, B second, C third ,novemberizing.ds.on.Quadruple<A, B, C, novemberizing.ds.on.Single<Z>> on){
+    public <A, B, C> Req(Observable<Z> observable, A first, B second, C third ,novemberizing.ds.on.Quadruple<A, B, C, novemberizing.ds.on.Single<Z>> on){
+        __observable = observable;
         __req = new On.Exec.Triple<>(Tuple.New(first, second, third), on, __ret);
     }
 
-    public Req(novemberizing.ds.func.Empty<Z> func){
+    public Req(Observable<Z> observable, novemberizing.ds.func.Empty<Z> func){
+        __observable = observable;
         __req = new Func.Exec.Empty<>(func, __ret);
     }
 
-    public <A> Req(A first, novemberizing.ds.func.Single<A, Z> func){
+    public <A> Req(Observable<Z> observable, A first, novemberizing.ds.func.Single<A, Z> func){
+        __observable = observable;
         __req = new Func.Exec.Single<>(Tuple.New(first), func, __ret);
     }
 
-    public <A, B> Req(A first, B second, novemberizing.ds.func.Pair<A, B, Z> func){
+    public <A, B> Req(Observable<Z> observable, A first, B second, novemberizing.ds.func.Pair<A, B, Z> func){
+        __observable = observable;
         __req = new Func.Exec.Pair<>(Tuple.New(first, second), func, __ret);
     }
 
-    public <A, B, C> Req(A first, B second, C third ,novemberizing.ds.func.Triple<A, B, C, Z> func){
+    public <A, B, C> Req(Observable<Z> observable, A first, B second, C third ,novemberizing.ds.func.Triple<A, B, C, Z> func){
+        __observable = observable;
         __req = new Func.Exec.Triple<>(Tuple.New(first, second, third), func, __ret);
     }
 
-
-    public void exec(){
+    protected void exec(){
         try {
             __req.exec();
         } catch(Exception e){
