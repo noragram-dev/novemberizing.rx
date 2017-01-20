65057bc975a0ae20b86ef926c97ecc7ca4f81c06
novemberizing <novemberizing@gmail.com>
2017-01-20 13:40:10 +0900

    Implement Req

--- src/main/java/novemberizing/rx/Res.java
+++ src/main/java/novemberizing/rx/Res.java
@@ -13,10 +13,28 @@ public class Res<T> {
     private Throwable __exception;
     private boolean __completed;
 
-    public Res(){
-        __out = null;
+    public Res(T out){
+        __out = out;
         __exception = null;
-        __completed = false;
+        __completed = true;
+    }
+
+    public Res(T out, boolean completed){
+        __out = out;
+        __exception = null;
+        __completed = completed;
+    }
+
+    public Res(T out, Throwable e){
+        __out = out;
+        __exception = e;
+        __completed = true;
+    }
+
+    public Res(T out, Throwable e, boolean completed){
+        __out = out;
+        __exception = e;
+        __completed = completed;
     }
 
     public T out(){ return __out; }
