071fc11eb965085936f97c819a44d696e656f74b
novemberizing <novemberizing@gmail.com>
2017-01-21 13:48:36 +0900

    Java Path Util

---
+++ src/main/java/novemberizing/util/Path.java
@@ -0,0 +1,72 @@
+package novemberizing.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 1. 21.
+ */
+public class Path {
+
+    private final String __full;
+    private LinkedList<String> __parents;
+    private String __self;
+    private String __name;
+    private String __extension;
+
+    public String full(){ return __full; }
+    public String v(){ return __self; }
+    public String name(){ return __name; }
+    public String extention(){ return __extension; }
+    public LinkedList<String> parents(){ return __parents; }
+
+    public boolean mkdir(){
+        String path = "/";
+        for(String dir : __parents){
+            path += dir;
+            File f = new File(path);
+            if(!f.mkdir()){
+                return false;
+            } else {
+                path += "/";
+            }
+        }
+        return true;
+    }
+
+    public boolean exist(){
+        File f = new File(__full);
+        return f.exists();
+    }
+
+    public boolean touch() throws IOException {
+        File f = new File(__full);
+        return f.createNewFile();
+    }
+
+    public Path(String s){
+        __full = s;
+        __parents = new LinkedList<>();
+        Collections.addAll(__parents, __full.split("[/]"));
+        __self = __parents.pollLast();
+        String[] splitted = __self.split("[.]");
+        if(splitted.length>1){
+            __extension = splitted[splitted.length-1];
+            __name = __self.substring(0, __self.length() - __extension.length());
+        } else {
+            __extension = null;
+        }
+    }
+
+    public Path(Path path){
+        __full = path.__full;
+        __parents = new LinkedList<>(path.__parents);
+        __self = path.__self;
+        __name = path.__name;
+        __extension = path.__extension;
+    }
+}
