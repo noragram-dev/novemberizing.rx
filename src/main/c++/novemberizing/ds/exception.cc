f47617ef35686b84d5b148d0ef6be38414acfca1
novemberizing <me@novemberizing.net>
2017-01-22 10:51:55 +0900

    dail

--- src/main/c++/novemberizing/ds/exception.cc
+++ src/main/c++/novemberizing/ds/exception.cc
@@ -1,5 +1,6 @@
 #ifndef   __NOVEMBERIZING_DS__EXCEPTION__INLINE__HH__
 #define   __NOVEMBERIZING_DS__EXCEPTION__INLINE__HH__
+
 #include <novemberizing/ds/exception.hh>
 
 namespace novemberizing { namespace ds {
