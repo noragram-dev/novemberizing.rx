af4f55545b7afb726f0ce12e1d399ab4c4aba626
novemberizing <novemberizing@gmail.com>
2017-01-23 13:25:14 +0900

    request once

--- src/main/java/novemberizing/rx/Req.java
+++ src/main/java/novemberizing/rx/Req.java
@@ -237,6 +237,57 @@ public class Req<Z> implements Executable {
     public Observable<Z> unsubscribe(Observer<Z> observer){ return __completionPort!=null ? __completionPort.unsubscribe(observer) : null; }
     public Observable<Z> unsubscribe(){ return __completionPort!=null ? __completionPort.unsubscribe() : null; }
 
+    public Observable<Z> once(novemberizing.ds.on.Single<Z> f){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onNext(Z o) {
+                f.on(o);
+                unsubscribe(this);
+            }
+        });
+    }
+
+    public Observable<Z> on(novemberizing.ds.on.Single<Z> f){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onNext(Z o) { f.on(o); }
+        });
+    }
+
+    public Observable<Z> exception(novemberizing.ds.on.Single<Throwable> f){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onError(Throwable e) { f.on(e); }
+        });
+    }
+
+    public Observable<Z> completion(novemberizing.ds.on.Empty f){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onComplete() { f.on(); }
+        });
+    }
+
+    public Observable<Z> on(novemberizing.ds.on.Single<Z> f, boolean once){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onNext(Z o) {
+                f.on(o);
+                if(once){ unsubscribe(this); }
+            }
+        });
+    }
+
+    public Observable<Z> exception(novemberizing.ds.on.Single<Throwable> f, boolean once){
+        return subscribe(new Subscribers.Just<Z>() {
+            @Override
+            public void onError(Throwable e) {
+                f.on(e);
+                if(once){ unsubscribe(this); }
+            }
+        });
+    }
+
 
     public <U> Sync<Z, U> sync(Single<Z, U> f){
         if(__completionPort==null){
