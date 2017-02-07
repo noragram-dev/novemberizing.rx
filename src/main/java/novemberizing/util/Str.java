1f01ff1679346e0cceba5420f61a8f90b160c00a
novemberizing <me@novemberizing.net>
2017-02-07 16:18:51 +0900

    hello

---
+++ src/main/java/novemberizing/util/Str.java
@@ -0,0 +1,25 @@
+package novemberizing.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 2. 7.
+ */
+@SuppressWarnings("unused")
+public class Str {
+    private static String From(BufferedReader reader){
+        StringBuilder builder = new StringBuilder();
+        String line;
+        try {
+            while((line = reader.readLine())!=null){
+                builder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
