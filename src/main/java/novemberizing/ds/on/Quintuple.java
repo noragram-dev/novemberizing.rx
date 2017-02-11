f7b528a74305eb257b444fcf58ce85f65f58759e
novemberizing <novemberizing@gmail.com>
2017-02-11 12:08:20 +0900

    e

---
+++ src/main/java/novemberizing/ds/on/Quintuple.java
@@ -0,0 +1,8 @@
+package novemberizing.ds.on;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 2. 11.
+ */
+public interface Quintuple<A, B, C, D, E> extends novemberizing.ds.On { void on(A first, B second, C third, D fourth, E fifth); }
